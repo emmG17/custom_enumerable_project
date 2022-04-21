89524ae8eaa0539476ad71654964030ff32831c8
Emmanuel Gonzalez <emm.gs17@gmail.com>
2022-04-21 00:40:17 -0500

    Add passed block verification

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,12 +1,12 @@
 module Enumerable
   # Your code goes here
   def my_all?(&block)
-    my_each { |item| return false unless block.call(item) }
+    my_each { |item| return false unless block.call(item) } if block_given?
     true
   end
 
   def my_any?(&block)
-    my_each { |item| return true if block.call(item) }
+    my_each { |item| return true if block.call(item) } if block_given?
     false
   end
 
@@ -22,32 +22,34 @@ module Enumerable
 
   def my_each_with_index(&block)
     idx = 0
-    my_each do |val|
-      block.call(val, idx)
-      idx += 1
+    if block_given?
+      my_each do |val|
+        block.call(val, idx)
+        idx += 1
+      end
     end
     self
   end
 
   def my_inject(initial = 0, &block)
-    my_each { |val| initial = block.call(initial, val) }
+    my_each { |val| initial = block.call(initial, val) } if block_given?
     initial
   end
 
   def my_map(&block)
     res = []
-    my_each { |val| res.append(block.call(val)) }
+    my_each { |val| res.append(block.call(val)) } if block_given?
     res
   end
 
   def my_none?(&block)
-    my_each { |val| return false if block.call(val) }
+    my_each { |val| return false if block.call(val) } if block_given?
     true
   end
 
   def my_select(&block)
     res = []
-    my_each { |val| res.append(val) if block.call(val) }
+    my_each { |val| res.append(val) if block.call(val) } if block_given?
     res
   end
 end
@@ -59,7 +61,7 @@ end
 class Array
   # Define my_each here
   def my_each(&block)
-    length.times { |i| block.call self[i] }
+    length.times { |i| block.call self[i] } if block_given?
     self
   end
 end
